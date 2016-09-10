5877b4f57f5c24124c5bc35b23cc0fd03ad28d4b
Christopher Comella <git.chrisco@gmail.com>
2016-09-10 17:25:38 -0600

    Implement and test 'next' and 'vote' functions (This was two different commits, but I did a $ git commit --amend to fix a typo in the commit message AFTER I had done a $ git add -A, so the 'vote' code was added to the 'next' code.)

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,7 +1,6 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
-
-import {setEntries} from '../src/core';
+import {setEntries, next, vote} from '../src/core';
 
 describe('application logic', () => {
 
@@ -11,6 +10,7 @@ describe('application logic', () => {
             const state = Map();
             const entries = List.of('Donnie Darko', 'Point Break');
             const nextState = setEntries(state, entries);
+
             expect(nextState).to.equal(Map({
                 entries: List.of('Donnie Darko', 'Point Break')
             }));
@@ -20,9 +20,73 @@ describe('application logic', () => {
             const state = Map();
             const entries = ['Donnie Darko', 'Point Break'];
             const nextState = setEntries(state, entries);
+
             expect(nextState).to.equal(Map({
                 entries: List.of('Donnie Darko', 'Point Break')
             }));
         });
-    })
+    });
+
+    describe('next', () => {
+        it('takes the next pair of entries up for vote', () => {
+            const state = Map({
+                entries: List.of('Donnie Darko', 'Point Break', '11:14')
+            });
+            const nextState = next(state);
+
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Donnie Darko', 'Point Break')
+                }),
+                entries: List.of('11:14')
+            }));
+        });
+    });
+
+    describe('vote', () => {
+        it('creates a tally for the voted entry', () => {
+            const state = Map({
+                vote: Map({
+                    pair: List.of('Donnie Darko', 'Point Break')
+                }),
+                entries: List()
+            });
+            const nextState = vote(state, 'Donnie Darko');
+
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Donnie Darko', 'Point Break'),
+                    tally: Map({
+                        'Donnie Darko': 1
+                    })
+                }),
+                entries: List()
+            }));
+        });
+
+        it('adds to existing tally for the voted entry', () => {
+            const state = Map({
+                vote: Map({
+                    pair: List.of('Donnie Darko', 'Point Break'),
+                    tally: Map({
+                        'Donnie Darko': 3,
+                        'Point Break': 2
+                    })
+                }),
+                entries: List()
+            });
+            const nextState = vote(state, 'Donnie Darko');
+
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Donnie Darko', 'Point Break'),
+                    tally: Map({
+                        'Donnie Darko': 4,
+                        'Point Break': 2
+                    })
+                }),
+                entries: List()
+            }));
+        });
+    });
 });
